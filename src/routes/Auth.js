d0099225b9909f9fc6bbcf9618705e4de306277d
kangkyeongseo <kks_big@naver.com>
2023-01-12 16:06:00 +0900

    Login Form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+        />
+        <input
+          onChange={onChange}
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+        />
+        <input type="submit" value="Login" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
