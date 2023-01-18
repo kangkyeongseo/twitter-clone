5c226fa21a8099b2551cd4196c4970603ac1862b
kangkyeongseo <kks_big@naver.com>
2023-01-18 10:54:37 +0900

    Get My Own Tweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,26 @@
-import React from "react";
-import { auth } from "fbase";
+import React, { useEffect } from "react";
+import { auth, db } from "fbase";
 import { useNavigate } from "react-router-dom";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const navigate = useNavigate();
   const onLogOutClick = async () => {
     await auth.signOut();
     navigate("/");
   };
+  const getMyTweets = async () => {
+    const q = query(
+      collection(db, "tweets"),
+      where("createdId", "==", userObj.uid),
+      orderBy("createdAt", "desc")
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => console.log(doc.data()));
+  };
+  useEffect(() => {
+    getMyTweets();
+  }, []);
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
